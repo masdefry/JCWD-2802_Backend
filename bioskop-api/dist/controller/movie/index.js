1212c467c1166c458c5d92a18310347f43f40f97
masdefry <muhammaddefryan@gmail.com>
2024-06-24 14:20:34 +0700

    Get Movies by Date and Time

--- bioskop-api/dist/controller/movie/index.js
+++ bioskop-api/dist/controller/movie/index.js
@@ -16,14 +16,46 @@ exports.GetMovies = void 0;
 const fs_1 = __importDefault(require("fs"));
 const GetMovies = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
+        const { status, date, time } = req.query;
         // Step-01 Read Data from db.json
-        const { movies } = JSON.parse(fs_1.default.readFileSync('./src/db/db.json', 'utf-8'));
-        // Step-02 Send Response
-        res.status(200).send({
-            error: false,
-            message: 'Get All Movies Success',
-            data: movies
-        });
+        const { movies, transactions } = JSON.parse(fs_1.default.readFileSync('./src/db/db.json', 'utf-8'));
+        if (!status && !date && !time)
+            return res.status(200).send({
+                error: false,
+                message: 'Get All Movies Success',
+                data: movies
+            });
+        if (status) {
+            const moviesByStatus = movies.filter((item) => {
+                return item.status === status.split('%').join(' ').toUpperCase();
+            });
+            return res.status(200).send({
+                error: false,
+                message: 'Get Movies by Status Success',
+                data: moviesByStatus
+            });
+        }
+        if (date && time) {
+            const moviesOnShowing = movies.filter((item) => {
+                return item.status === 'ON SHOWING';
+            });
+            moviesOnShowing.forEach((mov) => {
+                let totalBookSeat = 0;
+                transactions.forEach((trans) => {
+                    if (mov.id === trans.movies_id) {
+                        if (date === trans.date && time === trans.time) {
+                            totalBookSeat += trans.total_seat;
+                        }
+                    }
+                });
+                mov.seat_available = mov.total_seat - totalBookSeat;
+            });
+            res.status(200).send({
+                error: false,
+                message: 'Get Movies by Date & Time Success',
+                data: moviesOnShowing
+            });
+        }
     }
     catch (error) {
         console.log(error);
